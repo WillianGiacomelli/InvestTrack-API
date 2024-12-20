86d537ecc629fc81390d156c152c9456510e5ab5
Willian Giacomelli <willian2406@live.com>
2024-12-20 17:55:06 -0300

    modify quoteService

--- src/services/quotes/QuotesService.ts
+++ src/services/quotes/QuotesService.ts
@@ -1,12 +1,72 @@
 require('dotenv').config();
 
 class QuotesService {
+    private API_BASE_URL: string = process.env.BRAPI_URL;
     public async getStockQuotes() {
-        const API_BASE_URL: string = process.env.BRAPI_URL;
-        const API_URL: string = `${API_BASE_URL}/quote/list?type=stock&sortBy=change&sortOrder=asc&limit=30`;
+        const higherStockQuoteVariation = await this.getHigherStockQuoteVariation();
+        const lowerStockQuoteVariation = await this.getLowerStockQuoteVariation();
+        const lowerFundQuoteVariation = await this.getLowerFundQuoteVariation();
+        const higherFundQuoteVariation = await this.getHigherFundQuoteVariation();
 
+
+        const higherStocks = higherStockQuoteVariation
+            .filter((stock: any) => !stock.stock.endsWith('F'))
+            .slice(0, 5);
+  
+        const lowerStocks = lowerStockQuoteVariation
+            .filter((stock: any) => !stock.stock.endsWith('F'))
+            .slice(0, 5);
+
+        const higherFunds = higherFundQuoteVariation
+            .filter((stock: any) => stock.stock.endsWith('F'))
+            .slice(0, 5);
+
+        const lowerFunds = lowerFundQuoteVariation
+            .filter((stock: any) => stock.stock.endsWith('F'))
+            .slice(0, 5);
+  
+        const minStockLength = Math.min(higherStocks.length, lowerStocks.length);
+        const minFundLength = Math.min(higherFunds.length, lowerFunds.length);
+  
+        return {
+            stocks:{
+                higher: higherStocks.slice(0, minStockLength),
+                lower: lowerStocks.slice(0, minStockLength),
+            },
+            funds:{
+                higher: higherFunds.slice(0, minFundLength),
+                lower: lowerFunds.slice(0, minFundLength),
+            }
+        };
+    }
+
+    private getHigherStockQuoteVariation() {
+        const API_URL: string = `${this.API_BASE_URL}/quote/list?type=stock&sortBy=change&sortOrder=desc`;
+
+        return this.getQuotes(API_URL);
+    }
+
+    private getLowerStockQuoteVariation() {
+        const API_URL: string = `${this.API_BASE_URL}/quote/list?type=stock&sortBy=change&sortOrder=asc`;
+
+        return this.getQuotes(API_URL);
+    }
+
+    private getHigherFundQuoteVariation() {
+        const API_URL: string = `${this.API_BASE_URL}/quote/list?type=fund&sortBy=change&sortOrder=desc`;
+
+        return this.getQuotes(API_URL);
+    }
+
+    private getLowerFundQuoteVariation() {
+        const API_URL: string = `${this.API_BASE_URL}/quote/list?type=fund&sortBy=change&sortOrder=asc`;
+
+        return this.getQuotes(API_URL);
+    }
+
+    private async getQuotes(url: string) : Promise<any> {
         try{
-            const response = await fetch(API_URL,{
+            const response = await fetch(url,{
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -24,7 +84,9 @@ class QuotesService {
                 throw new Error('Dados não encontrados');
             }
 
-            const stocks = data.stocks.filter((stock: any) => !stock.stock.endsWith('F')).slice(0, 10);
+            const stocks = data.stocks;
+                                                                        ;
+                                                                            
             return stocks;
         } catch (error) {
             throw new Error(`Não foi possível buscar os dados da Brapi ${error.message}`);
