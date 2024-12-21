8395c8a66c09212dc75b563202beb9284ea2badd
Willian Giacomelli <willian2406@live.com>
2024-12-20 22:32:34 -0300

    changes quote service

--- src/services/quotes/QuotesService.ts
+++ src/services/quotes/QuotesService.ts
@@ -1,12 +1,14 @@
 require('dotenv').config();
 
 class QuotesService {
-    private API_BASE_URL: string = process.env.BRAPI_URL;
+    private BRAPI_API_BASE_URL: string = process.env.BRAPI_URL;
+    private COIN_MARKET_CAP_API_BASE_URL: string = process.env.COIN_MARKET_CAP_URL;
     public async getStockQuotes() {
         const higherStockQuoteVariation = await this.getHigherStockQuoteVariation();
         const lowerStockQuoteVariation = await this.getLowerStockQuoteVariation();
         const lowerFundQuoteVariation = await this.getLowerFundQuoteVariation();
         const higherFundQuoteVariation = await this.getHigherFundQuoteVariation();
+        const cryptoQuotes = await this.getCryptoQuotes();
 
 
         const higherStocks = higherStockQuoteVariation
@@ -36,35 +38,42 @@ class QuotesService {
             funds:{
                 higher: higherFunds.slice(0, minFundLength),
                 lower: lowerFunds.slice(0, minFundLength),
-            }
+            },
+            cryptos: cryptoQuotes.slice(0, 20)
         };
     }
 
     private getHigherStockQuoteVariation() {
-        const API_URL: string = `${this.API_BASE_URL}/quote/list?type=stock&sortBy=change&sortOrder=desc`;
+        const API_URL: string = `${this.BRAPI_API_BASE_URL}/quote/list?type=stock&sortBy=change&sortOrder=desc`;
 
-        return this.getQuotes(API_URL);
+        return this.fetchQuotes(API_URL);
     }
 
     private getLowerStockQuoteVariation() {
-        const API_URL: string = `${this.API_BASE_URL}/quote/list?type=stock&sortBy=change&sortOrder=asc`;
+        const API_URL: string = `${this.BRAPI_API_BASE_URL}/quote/list?type=stock&sortBy=change&sortOrder=asc`;
 
-        return this.getQuotes(API_URL);
+        return this.fetchQuotes(API_URL);
     }
 
     private getHigherFundQuoteVariation() {
-        const API_URL: string = `${this.API_BASE_URL}/quote/list?type=fund&sortBy=change&sortOrder=desc`;
+        const API_URL: string = `${this.BRAPI_API_BASE_URL}/quote/list?type=fund&sortBy=change&sortOrder=desc`;
 
-        return this.getQuotes(API_URL);
+        return this.fetchQuotes(API_URL);
     }
 
     private getLowerFundQuoteVariation() {
-        const API_URL: string = `${this.API_BASE_URL}/quote/list?type=fund&sortBy=change&sortOrder=asc`;
+        const API_URL: string = `${this.BRAPI_API_BASE_URL}/quote/list?type=fund&sortBy=change&sortOrder=asc`;
 
-        return this.getQuotes(API_URL);
+        return this.fetchQuotes(API_URL);
     }
 
-    private async getQuotes(url: string) : Promise<any> {
+    private async getCryptoQuotes() {
+        const API_URL: string = `${this.COIN_MARKET_CAP_API_BASE_URL}/v1/cryptocurrency/listings/latest?convert=BRL`;
+
+        return this.fetchCryptoQuotes(API_URL);
+    }
+
+    private async fetchQuotes(url: string) : Promise<any> {
         try{
             const response = await fetch(url,{
                 method: 'GET',
@@ -90,6 +99,36 @@ class QuotesService {
             return stocks;
         } catch (error) {
             throw new Error(`Não foi possível buscar os dados da Brapi ${error.message}`);
+        }
+    }
+
+    private async fetchCryptoQuotes(url: string) : Promise<any> {
+        try{
+            const response = await fetch(url,{
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CMC_PRO_API_KEY': process.env.COIN_MARKET_CAP_TOKEN
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error(`Erro ao buscar dados: ${response.status} - ${response.statusText}`);
+            }
+
+            const data = await response.json();
+
+            if(!data.data){
+                throw new Error('Dados não encontrados');
+            }
+
+            const crypto = data.data;
+                            
+            const sortedByHighMarketCap = crypto.sort((a, b) => b.quote.BRL.market_cap - a.quote.BRL.market_cap);;
+                                                                            
+            return sortedByHighMarketCap;
+        } catch (error) {
+            throw new Error(`Não foi possível buscar os dados da CoinMarketCap ${error.message}`);
         }
     }
 }
