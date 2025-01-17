7b11a2b4b6703b8f1776c81df94f031a69ffae08
Willian Giacomelli <willian2406@live.com>
2025-01-16 22:47:36 -0300

    feat: adds total variation percent

--- src/services/investments/InvestmentService.ts
+++ src/services/investments/InvestmentService.ts
@@ -115,7 +115,8 @@ class InvestmentService  {
             const investmentsOverview = {
                 paid,
                 currentTotalValue,
-                totalVariation
+                totalVariation,
+                percentTotalVariation: ((currentTotalValue - paid) / paid) * 100,
             };
 
             return {
